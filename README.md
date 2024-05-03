9c4d91aafc734800e0cffc164afd3f5bf8ccba88
Md Fuad Islam <fuadislamofficial@gmail.com>
2024-05-03 18:10:36 +0600

    Done

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Depoly: https://task-4-jcel.onrender.com
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
+Demo: 
 
 * ...
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,13 +8,24 @@ class HomeController < ApplicationController
 
     if params[:block]
       User.unscoped.where(id: selected_user_ids).update_all(status: "blocked")
+      flash[:notice] = "Action was successful!"
+      if selected_user_ids.map(&:to_i).include?(current_user.id)
+        sign_out_and_redirect(current_user)
+      else
+        redirect_to home_index_path
+      end
     elsif params[:delete]
       User.unscoped.where(id: selected_user_ids).destroy_all
+      flash[:notice] = "Action was successful!"
+      if selected_user_ids.map(&:to_i).include?(current_user.id)
+        sign_out_and_redirect(current_user)
+      else
+        redirect_to home_index_path
+      end
     elsif params[:unblock]
       User.unscoped.where(id: selected_user_ids).update_all(status: "active")
+      flash[:notice] = "Action was successful!"
+      redirect_to home_index_path
     end
-
-    flash[:notice] = "Action was successful!"
-    redirect_to home_index_path
   end
 end
